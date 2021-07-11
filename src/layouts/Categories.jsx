22e30c77bfaf4c6183efba794286febe61c62d4f
arawna <alihocaoglu1220@gmail.com>
2021-07-11 15:02:54 +0300

    categories active added

--- src/layouts/Categories.jsx
+++ src/layouts/Categories.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Icon, Menu } from 'semantic-ui-react'
 
 export default function Categories() {
+
+  const location = useLocation().pathname;
+
   return (
     <div>
       <Menu fluid compact icon="labeled" vertical>
-        <Menu.Item as={Link} to={"/jobads"}>
+        <Menu.Item as={Link} to={"/jobads"} active={location==="/jobads" || location==="/"}>
             <Icon name="list" />
             İş ilanları        
         </Menu.Item>
 
-        <Menu.Item as={Link} to={"/employers"}>
+        <Menu.Item as={Link} to={"/employers"} active={location==="/employers"}>
             <Icon name="factory" />
             İş verenler       
         </Menu.Item>
 
-        <Menu.Item as={Link} to={"/candidates"}>
+        <Menu.Item as={Link} to={"/candidates"} active={location==="/candidates"}>
           <Icon name="user" />
           Kullanıcılar
         </Menu.Item>
 
-        <Menu.Item as={Link} to={"/cvs"}>
+        <Menu.Item as={Link} to={"/cvs"} active={location==="/cvs"}>
           <Icon name="wordpress forms" />
           Cvler
         </Menu.Item>
